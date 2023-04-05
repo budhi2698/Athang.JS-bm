392abf5146ea609434f24f887e9ded62c11a9d4c
unknown <budhi2698>
2023-04-05 09:36:05 -0700

    update2

---
+++ javascript/ass.js
@@ -0,0 +1,4 @@
+document.write("hello world");
+//document.getElementById("demo").innerHTML="sorry world";
+ window.alert("My page");
+console.log('hello world');
---
+++ javascript/new.js
@@ -0,0 +1,5 @@
+document.write("hello world");
+//document.getElementById("demo").innerHTML="sorry world";
+ window.alert("My page");
+console.log('hello world');
+
